3dcc1388d3b381aa155318eca88affa3df5918c8
Michael Huttner <michael@mhuttner.com>
2023-10-04 09:57:46 +0200

    add stash pop

--- urnc/edit.py
+++ urnc/edit.py
@@ -3,12 +3,12 @@ import click
 
 
 import urnc.util as util
+import urnc.student as student
 
 @click.command(help="Edit the course, switch to main branch")
 @click.pass_context
 def edit(ctx):
     repo = util.get_git_repo(ctx)
-    remote = repo.remote("origin")
 
     main_branch = "main"
     if not util.branch_exists(repo, main_branch):
@@ -16,4 +16,7 @@ def edit(ctx):
 
     repo.git.fetch("--all")
     repo.git.switch(main_branch)
+    idx = student.find_urnc_stash(repo)
+    if(idx):
+        repo.git.stash("pop", idx)
 
