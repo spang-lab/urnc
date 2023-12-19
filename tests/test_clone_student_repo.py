1397d1557f3b4504e999ad0f816b6b9de1cdb9ee
Tobias Schmidt <tobias2.schmidt@ukr.de>
2023-12-19 17:05:53 +0100

    Added tests for urnc.ci.clone_student_repo

--- tests/test_clone_student_repo.py
+++ tests/test_clone_student_repo.py
@@ -1,45 +1,75 @@
 from os import chdir, getcwd
 from os.path import abspath, isdir
-from shutil import rmtree
 
 import git
 
 import urnc
+from urnc.test import rmtree
 
 cwd = getcwd()
-test_course = "tests/courses/minimal"
-test_course_git = "tests/courses/minimal/.git"
-test_course_student = f"{abspath(test_course)}-student"
+minimal_course = abspath("tests/courses/minimal")
+minimal_course_git = f"{minimal_course}/.git"
+minimal_course_student = f"{minimal_course}-student"
+example_course = abspath("tests/courses/urnc-example-course")
+example_course_url = "https://github.com/spang-lab/urnc-example-course"
+example_course_git = f"{example_course}/.git"
+example_course_student = f"{example_course}-public"
 
 def setup_module(module):
     """Make sure the used test course is a git repo, as we don't commit the .git folders from the test courses. This function is run once before all tests in this module."""
-    if isdir(test_course) and not isdir(test_course_git):
-        git.Repo.init(test_course)
+    if isdir(minimal_course) and not isdir(minimal_course_git):
+        git.Repo.init(minimal_course)
+    if isdir(example_course):
+        if not isdir(example_course_git):
+            rmtree(example_course)
+            git.Repo.clone_from(url=example_course_url, to_path=example_course)
+    else:
+        git.Repo.clone_from(url=example_course_url, to_path=example_course)
+    git.Repo(example_course).git.checkout("9c277cdc7c7985f433f2c7f6fabcba1eefd2f844")
+
 
 def test_clone_student_repo_url_missing_repo_missing():
     try:
-        if isdir(test_course_student):
-            rmtree(test_course_student)
-        chdir("tests/courses/minimal")
+        if isdir(minimal_course_student):
+            rmtree(minimal_course_student)
+        chdir(minimal_course)
         config = urnc.util.read_config()
         repo = urnc.ci.clone_student_repo(config)
-        assert repo.working_dir == test_course_student
+        assert repo.working_dir == minimal_course_student
     finally:
         chdir(cwd)
 
 def test_clone_student_repo_url_missing_repo_exists():
     try:
-        assert isdir(test_course_student), "The student repo should already exist, as it was created by the previous test"
-        chdir("tests/courses/minimal")
+        msg = "Student repo should exist, as it was created by previous test"
+        assert isdir(minimal_course_student), msg
+        chdir(minimal_course)
         config = urnc.util.read_config()
         repo = urnc.ci.clone_student_repo(config)
-        assert repo.working_dir == test_course_student
+        assert repo.working_dir == minimal_course_student
     finally:
         chdir(cwd)
 
 def test_clone_student_repo_url_exists_repo_missing():
-    pass
+    try:
+        if isdir(example_course_student):
+            rmtree(example_course_student)
+        chdir(example_course)
+        config = urnc.util.read_config()
+        repo = urnc.ci.clone_student_repo(config)
+        assert repo.working_dir == example_course_student
+    finally:
+        chdir(cwd)
 
 def test_clone_student_repo_url_exists_repo_exists():
-    pass
+    try:
+        msg = "Student repo should exist, as it was created by previous test"
+        assert isdir(example_course_student), msg
+        chdir(example_course)
+        config = urnc.util.read_config()
+        repo = urnc.ci.clone_student_repo(config)
+        assert repo.working_dir == example_course_student
+    finally:
+        chdir(cwd)
+
 
