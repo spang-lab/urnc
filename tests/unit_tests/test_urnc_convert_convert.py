25f91f0c91d2208a4f08c1002e50d268940e290f
Tobias Schmidt <tobias2.schmidt@ukr.de>
2024-01-02 21:38:44 +0100

    Added testcases and documentation for solution parameter

--- tests/unit_tests/test_urnc_convert_convert.py
+++ tests/unit_tests/test_urnc_convert_convert.py
@@ -50,3 +50,55 @@ def test_convert__inDir_solF_forceF_dryF_outExistF_solExistF():
     urnc.convert.convert(input=input, output=outputs_dir)
     cmp = filecmp.dircmp(outputs_dir, expected)
     assert all((cmp.diff_files == [], cmp.left_only == [], cmp.right_only == []))
+
+# Solution True
+
+def test_convert__inNB_solT_forceF_dryF_outExistF_solExistF():
+    test_case = 4
+    outputs_dir = conftest.init_outputs_dir(test_module, test_function, test_case)
+    expects_dir = "tests/expected/minimal-course-converted/lectures"
+    input = "tests/inputs/minimal-course/lectures/urnc.ipynb"
+    outputs = [f"{outputs_dir}/urnc.ipynb", f"{outputs_dir}/urnc-solution.ipynb"]
+    expects = [f"{expects_dir}/urnc.ipynb", f"{expects_dir}/urnc-solution.ipynb"]
+    urnc.convert.convert(input=input, output=outputs_dir, solution=outputs_dir)
+    assert all([
+        filecmp.cmp(outputs[0], expects[0]),
+        filecmp.cmp(outputs[1], expects[1])
+    ])
+
+def test_convert__inNB_solT_forceF_dryF_outExistT_solExistT():
+    test_case = 5
+    outputs_dir = conftest.init_outputs_dir(test_module, test_function, test_case)
+    input = "tests/inputs/minimal-course/lectures/urnc.ipynb"
+    outputs = [f"{outputs_dir}/urnc.ipynb", f"{outputs_dir}/urnc-solution.ipynb"]
+    open(outputs[0], "w").write("abc")
+    open(outputs[1], "w").write("123")
+    urnc.convert.convert(input=input, output=outputs_dir, solution=outputs_dir)
+    assert all([
+        open(outputs[0], "r").read() == "abc",
+        open(outputs[1], "r").read() == "123"
+    ])
+
+def test_convert__inNB_solT_forceT_dryF_outExistT_solExistT():
+    test_case = 6
+    outputs_dir = conftest.init_outputs_dir(test_module, test_function, test_case)
+    expects_dir = "tests/expected/minimal-course-converted/lectures"
+    input = "tests/inputs/minimal-course/lectures/urnc.ipynb"
+    outputs = [f"{outputs_dir}/urnc.ipynb", f"{outputs_dir}/urnc-solution.ipynb"]
+    expects = [f"{expects_dir}/urnc.ipynb", f"{expects_dir}/urnc-solution.ipynb"]
+    open(outputs[0], "w").write("abc")
+    open(outputs[1], "w").write("123")
+    urnc.convert.convert(input=input, output=outputs_dir, solution=outputs_dir)
+    assert all([
+        filecmp.cmp(outputs[0], expects[0]),
+        filecmp.cmp(outputs[1], expects[1])
+    ])
+
+def test_convert__inDir_solT_forceF_dryF_outExistF_solExistF():
+    test_case = 7
+    outputs_dir = conftest.init_outputs_dir(test_module, test_function, test_case)
+    input = "tests/inputs/minimal-course/lectures"
+    expected = "tests/expected/minimal-course-converted/lectures"
+    urnc.convert.convert(input=input, output=outputs_dir)
+    cmp = filecmp.dircmp(outputs_dir, expected)
+    assert all((cmp.diff_files == [], cmp.left_only == [], cmp.right_only == []))
--- urnc/convert.py
+++ urnc/convert.py
@@ -1,8 +1,12 @@
 """Create student version of one or more Jupyter notebooks"""
 
 import os
+from pathlib import Path
+from typing import List, Optional, Union
 
 import nbformat
+import traitlets
+import sys
 from nbconvert.exporters.notebook import NotebookExporter
 from nbconvert.preprocessors.clearoutput import ClearOutputPreprocessor
 from traitlets.config import Config
@@ -15,70 +19,203 @@ from urnc.preprocessor.remove_solutions import RemoveSolutions
 
 def convert(input: str = ".",
             output: str = "out/",
+            solution: Optional[str] = None,
             force: bool = False,
             dry_run: bool = False) -> None:
     """Convert notebooks.
 
     Args:
         input: The input path. Can be either the path to a single ipynb file or the path to a directory containing ipynb files.
-        output: The output path. Must be a directory. Every input notebook will be copied to the output directory and converted there. If the output directory does not exist, it will be created.
+        output: Path for storing the student notebooks with solutions removed. Directories will be created if necessary. If `output` is None, the conversion will be skipped. Supports the following placeholder variables: `nb_path`, `nb_dir`, `nb_root`, `nb_relpath`, `nb_dir_relpath`, `nb_name`, `nb_basename`, `nb_ext`. For details see section "Details" below.
+        solution: Path for storing the student notebooks with solutions. Directories will be created if necessary. If `solution` is None, the conversion will be skipped. Supports the following placeholder variables: `nb_path`, `nb_dir`, `nb_root`, `nb_relpath`, `nb_dir_relpath`, `nb_name`, `nb_basename`, `nb_ext`. For details see section "Details" below.
         force: If true, existing output files will be overwritten.
         dry_run: If true, input files will be converted but not written to disk. This is useful for checking if the conversion works without changing any files.
 
     Examples:
-        convert_notebooks("/path/to/notebooks", "/path/to/output", force=True)
-        convert_notebooks("/path/to/single_notebook.ipynb", "/path/to/output", dry_run=True)
+        convert("/path/to/single_notebook.ipynb", "/path/to/output", dry_run=True)
+        convert("/path/to/notebooks", "/path/to/output", force=True)
+
+    Details:
+        A call like `convert("C:/Users/max/mycourse", ...)` will search `C:/Users/max/mycourse` recursively for notebooks and convert each notebook to student versions. To be able to specify output paths in a simple yet flexible way, the following placeholders are provided: `nb_path`, `nb_dir`, `nb_root`, `nb_relpath`, `nb_dir_relpath`, `nb_name`, `nb_basename`, `nb_ext`. To understand what these variables mean, consider the following folder structure:
+
+        C:/Users/max/mycourse
+        ├── lectures
+        │   ├── week1
+        │   │   ├── lecture1.ipynb
+        │   │   └── lecture2.ipynb
+        │   └── week2
+        │       ├── lecture3.ipynb
+        │       └── lecture4.ipynb
+        └── assignments
+            ├── week1.ipynb
+            └── week2.ipynb
+
+        The above course contains the following six notebooks, lecture1.ipynb, lecture2.ipynb, lecture3.ipynb, lecture4.ipynb, week1.ipynb and week2.ipynb. The following table shows the value of each placeholder variable exemplarily for notebook `C:/Users/max/mycourse/lectures/week2/lecture1.ipynb`:
+
+        | nb_path        | C:/Users/max/mycourse/lectures/week2/lecture1.ipynb |
+        | nb_dir         | C:/Users/max/mycourse/lectures/week2                |
+        | nb_root        | C:/Users/max/mycourse                               |
+        | nb_relpath     |                       lectures/week2/lecture1.ipynb |
+        | nb_dir_relpath |                       lectures/week2/               |
+        | nb_name        |                                      lecture1.ipynb |
+        | nb_basename    |                                      lecture1       |
+        | nb_ext         |                                               ipynb |
+
+        I.e. the following function call
+
+        convert(
+            input = "C:/Users/max/mycourse",
+            output = "C:/converted/{nb_relpath}",
+            solution = "C:/converted/{nb_dir_relpath}/solutions/{nb_name}",
+        )
+
+        would create the following output structure:
+
+        C:/converted
+        ├── lectures
+        │   ├── week1
+        │   │   ├── lecture1.ipynb
+        │   │   ├── lecture1.ipynb
+        │   │   └── solutions
+        │   │       └── lecture1.ipynb
+        │   │       └── lecture2.ipynb
+        │   └── week2
+        │       ├── lecture3.ipynb
+        │       ├── lecture4.ipynb
+        │       └── solutions
+        │           └── lecture3.ipynb
+        │           └── lecture4.ipynb
+        └── assignments
+            ├── week1.ipynb
+            ├── week2.ipynb
+            └── solutions
+                ├── week1.ipynb
+                └── week2.ipynb
+
+        To make specifying output paths even easier and to maintain backwards compatibility, the following shortcuts are provided:
+
+        1. If `output` does not end with `.ipynb` and does not contain any placeholders, the provided path will be interpreted as a directory and the notebooks will be saved in `{output}/{nb_relpath}`, i.e. the following calls are equivalent:
+
+           convert('mycourse', output = 'out')
+           convert('mycourse', output = 'out/{nb_relpath}')
+
+        2. If `solution` does not end with `.ipynb` and does not contain any placeholders, the provided path will be interpreted as a directory and the notebooks will be saved in `{solution}/{nb_dir_relpath}/{nb_basename}-solution.{nb_ext}`, i.e. the following calls are equivalent:
+
+            convert('mycourse', solution = 'out')
+            convert('mycourse', solution = 'out/{nb_dir_relpath}/{nb_basename}-solution.{nb_ext}')
     """
     log.log(f"Converting notebooks in {input} to {output}")
-    paths = []
-    folder = input
+    input_folder = os.path.dirname(input) if os.path.isfile(input) else input
+
     if os.path.isfile(input):
-        paths.append(input)
-        folder = os.path.dirname(input)
-    else:
+        input_paths = [input]
+    elif os.path.isdir(input):
+        input_paths = []
         for root, dirs, files in os.walk(input, topdown=True):
             dirs[:] = [d for d in dirs if not d[0] == "."]
             for file in files:
                 if file.lower().endswith(".ipynb"):
-                    paths.append(os.path.join(root, file))
-
-    if len(paths) == 0:
-        log.warn("No notebooks found in input %s" % input)
+                    input_paths.append(os.path.join(root, file))
+    else:
+        log.warn(f"Input path {input} does not exist")
+        return
+    if len(input_paths) == 0:
+        log.warn(f"No notebooks found in {input}")
         return
 
-    c = Config()
-    c.NotebookExporter.preprocessors = [
-        Linter,
-        AddTags,
-        RemoveSolutions,
-        ClearOutputPreprocessor,
-    ]
-    converter = NotebookExporter(config=c)
+    output_paths = []
+    solution_paths = []
+    for input_path in input_paths:
 
-    for file in paths:
-        opath = os.path.relpath(file, folder)
-        out_file = None
-        out_dir = None
-        if output is not None:
-            out_file = os.path.join(output, opath)
-            out_dir = os.path.dirname(out_file)
-            os.makedirs(out_dir, exist_ok=True)
-        if out_file is None:
-            log.log(f"Checking {file}")
-        elif os.path.isfile(out_file):
-            if not force:
-                log.log(f"Skipping {file} because {out_file} already exists")
-                continue
-            else:
-                log.log(f"Overwriting {out_file}")
-        else:
-            log.log(f"Converting '{file}' into '{out_file}'")
-        notebook = nbformat.read(file, as_version=4)
-        resources = {}
-        resources["path"] = file
-        (output_text, _) = converter.from_notebook_node(notebook, resources)
+        output_path = None # CONTINUE HERE
 
-        if (out_file is None or dry_run):
-            continue
-        with open(out_file, "w", newline='\n') as f:
-            f.write(output_text)
+        # Print status
+    for ip, op, sp in zip(input_paths, output_paths, solution_paths):
+        _convert_nb(ip, op, sp, force, dry_run)
+
+
+def _convert_nb(input: Union[str, Path],
+                output: Optional(Union[str, Path]) = None,
+                solution: Optional(Union[str, Path]) = None,
+                force: bool = False,
+                dryrun: bool = False) -> None:
+    """Convert a single notebook to its student version with and/or without solutions removed.
+
+    Args:
+        input: Path of input notebook.
+        output: Path for storing the student notebook without solutions. If the parent directory does not exist, it will be created. If output is None, the conversion will be skipped.
+        solution: Path for storing the student notebook with solutions. If the parent directory does not exist, it will be created. If solution is None, the conversion will be skipped.
+        force: Overwrite existing output files without asking?
+        dryrun: Skip writing to disk after performing conversions? Useful for testing without file changes.
+
+    Examples:
+        # Create student version with solutions removed/kept from dummy.ipynb
+        _convert_nb("dummy.ipynb", output="dummy-converted.ipynb")
+        _convert_nb("dummy.ipynb", solution="dummy-solution.ipynb")
+
+        # Same as above but in one line
+        _convert_nb("dummy.ipynb", output="dummy-converted.ipynb", solution="dummy-solution.ipynb")
+
+        # Absolute paths work as well
+        _convert_nb("C:/Users/max/dummy.ipynb", solution="C:/Users/max/dummy-solution.ipynb")
+
+        # Overwrite existing files without asking
+        _convert_nb("dummy.ipynb", output="dummy-converted.ipynb", force=True)
+
+        # Don't write converted files to disk
+        _convert_nb("dummy.ipynb", output="dummy-converted.ipynb", dryrun=True)
+    """
+    in_path = Path(input)
+    out_path = Path(output) if output else None
+    sol_path = Path(solution) if solution else None
+    if out_path or sol_path:
+        log.log(f"Reading {in_path}")
+        in_text = nbformat.read(in_path, as_version=4)
+        resources = {"path": in_path}
+    if out_path:
+        out_config = traitlets.config.Config()
+        out_config.NotebookExporter.preprocessors = [Linter, AddTags, RemoveSolutions, ClearOutputPreprocessor]
+        out_converter = NotebookExporter(out_config)
+        log.log(f"Converting {in_path} to student version without solutions")
+        out_text = out_converter.from_notebook_node(in_text, resources)[0]
+        if not dryrun:
+            write(text = out_text, path = out_path, force = force)
+    if sol_path:
+        sol_config = traitlets.config.Config()
+        sol_config.NotebookExporter.preprocessors = [AddTags, ClearOutputPreprocessor]
+        sol_converter = NotebookExporter(sol_config)
+        log.log(f"Converting {in_path} to student with solutions")
+        sol_text = sol_converter.from_notebook_node(in_text, resources)[0]
+        if not dryrun:
+            write(text = sol_text, path = sol_path, force = force)
+
+
+def write(text: str, path: str, force: bool = False) -> None:
+    """
+    Write text to path.
+    If the parent directory does not exist, it will be created.
+    If the path exists already and force is False, the user will be asked if they want to overwrite the file.
+    If stdout is not a tty, a warning message will be printed instead.
+    If force is True, the file will be overwritten without asking.
+
+    Args:
+        text: The text to write.
+        path: The path of the file to write to. If the parent directory does not exist, it will be created.
+        force: Overwrite existing files without asking?
+    """
+    path = Path(path)
+    path.parent.mkdir(parents=True, exist_ok=True)
+    if not path.exists() or force:
+        log.log(f"Writing {path}.")
+        with open(path, "w", newline="\n") as f:
+            f.write(text)
+        return
+    if not sys.stdout.isatty():
+        log.log(f"Skipped writing {path} because it exists already and force is False.")
+        return
+    overwrite = ''
+    while overwrite.lower() not in ['y', 'n']:
+        overwrite = input(f"File {path} exists. Overwrite? (y/n): ")
+    if overwrite.lower() == 'y':
+        with open(path, "w", newline="\n") as f:
+            f.write(text)
