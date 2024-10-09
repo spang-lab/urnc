a43f02d24eef8f7433de6991cc1440bddbd40630
Michael Huttner <michael@mhuttner.com>
2024-10-09 14:41:16 +0200

    fix test

--- tests/unit_tests/test_urnc_main_version.py
+++ tests/unit_tests/test_urnc_main_version.py
@@ -2,11 +2,12 @@ import re
 from subprocess import run
 import sys
 
+
 def test_version_command():
-    cmd = f"{sys.executable} -m urnc --version".split()
+    cmd = f"{sys.executable} -m urnc version --self".split()
     proc = run(cmd, capture_output=True, encoding="utf-8")
     errors = []
-    semver = r'^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$' # https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
+    semver = r"^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$"  # https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
 
     # Check results
     if proc.returncode != 0:
@@ -17,5 +18,7 @@ def test_version_command():
         errors.append(f"Version should be in semver format but is: {proc.stdout}")
     for error in errors:
         print(error)
-    n_errors = len(errors) # Store in extra var to get nicer pytest output in case of errors
+    n_errors = len(
+        errors
+    )  # Store in extra var to get nicer pytest output in case of errors
     assert n_errors == 0
