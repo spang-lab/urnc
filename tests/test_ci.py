7d93b13929e9727b2338692b924b11bb008b141c
Tobias Schmidt <tobias.schmidt331@gmail.com>
2025-05-14 01:05:22 +0200

    Use new init in test_ci

--- tests/test_ci.py
+++ tests/test_ci.py
@@ -1,47 +1,31 @@
 from pathlib import Path
-import git
 import tempfile
 
 import nbformat
 import urnc
-from urnc.config import WriteMode
 import urnc.preprocessor.util as util
 
 
 def test_ci():
-    tmp = tempfile.TemporaryDirectory()
-    path = Path(tmp.name)
-    repo_path = path / "repo"
-    d_config = urnc.config.default_config(path)
-    repo = urnc.init.init(d_config, "repo")
+    
+    tmp = Path(tempfile.mkdtemp())
+    course_name = "Example Course"
+    admin_path = tmp / "example-course-admin"
+    admin_url = tmp / "example-course-admin.git"
+    student_path = tmp / "example-course"
+    student_url = tmp / "example-course.git"
+    urnc.init.init(course_name, admin_path, admin_url, student_url)
 
-    remote_path = path / "remote.git"
-    remote_path.mkdir()
-    git.Repo.init(remote_path, bare=True, initial_branch="main")
-    origin = repo.create_remote("origin", str(remote_path))
-    origin.push(refspec="main:main")
-
-    student_remote = path / "student.git"
-    student_remote.mkdir()
-    git.Repo.init(student_remote, bare=True, initial_branch="main")
-
-    config = urnc.config.read(repo_path)
-    config["git"]["student"] = str(student_remote)
-    config["git"]["output_dir"] = "out"
-    config["convert"]["write_mode"] = WriteMode.OVERWRITE
+    config = urnc.config.read(admin_path)
+    config["convert"]["write_mode"] = "overwrite"
     config["ci"]["commit"] = True
     urnc.ci.ci(config)
+    urnc.pull.pull(student_url, student_path, "main", 1)
 
-    student_repo_path = path / "student"
-    urnc.pull.pull(str(student_remote), str(student_repo_path), "main", 1)
-
-    assert (student_repo_path / "example.ipynb").is_file()
-    assert not (student_repo_path / "config.yaml").exists()
-
-    nb = nbformat.read(str(student_repo_path / "example.ipynb"), as_version=4)
+    nb = nbformat.read(student_path / "example.ipynb", as_version=4)
     cells = nb["cells"]
     print(cells)
     assert util.has_tag(cells[2], "assignment")
     assert util.has_tag(cells[2], "assignment-start")
     assert util.has_tag(cells[3], "assignment")
-    tmp.cleanup()
+    assert not (student_path / "config.yaml").exists()
