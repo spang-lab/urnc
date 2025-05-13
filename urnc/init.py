6d99c2c22743f908c378b172370c91ae0f857d1b
Tobias Schmidt <tobias.schmidt331@gmail.com>
2025-05-14 00:21:24 +0200

    Refactor init.py. Add url args to init().

--- urnc/init.py
+++ urnc/init.py
@@ -1,42 +1,89 @@
+import os
 import re
-import click
-from nbconvert.exporters.notebook import NotebookExporter
-import urnc
 import textwrap
-import nbformat
-import git
 from pathlib import Path
+from typing import Any, List, Mapping, TypedDict, Union
+
+import click
+import git
+from nbconvert.exporters.notebook import NotebookExporter
+from nbformat import NotebookNode
+from nbformat.v4 import new_code_cell, new_markdown_cell, new_notebook
 from ruamel.yaml import YAML
 
+
 from urnc.logger import log
+
+
+example_config = {
+    "name": "Example Course",
+    "semester": "semester",
+    "description": "Description for your course",
+    "version": "0.1.0",
+    "authors": [
+        {
+            "name": "Your Name",
+            "email": "YourEmail@host.com",
+        }
+    ],
+    "git": {
+        "student": "Link to student repo",
+        "output_dir": "out",
+        "exclude": [
+            "config.yaml",
+            "container/",
+        ],
+    },
+}
+
+example_gitignore = textwrap.dedent(
+    """
+    out/
+    .ipynb_checkpoints/
+    .jupyter/
+    .vscode/
+    .DS_Store
+    """
+).strip()
+
+
+example_notebook = new_notebook(cells=[
+    new_markdown_cell(
+        "# Example Notebook\n\n" +
+        "urnc detects cells by checking for keywords in markdown headers"
+    ),
+    new_code_cell(
+        "print('This is a normal code cell.')"
+    ),
+    new_markdown_cell(
+        "## Assignment 1\n\n" +
+        "Assignments are detected by urnc."
+    ),
+    new_markdown_cell(
+        "Cells after an assignment headers are part of the assignment."
+    ),
+    new_markdown_cell(
+        "## Solution\n\n" +
+        "Solutions are removed by the urnc ci command."
+    ),
+    new_code_cell(
+        "## Solution\n\n" +
+        "print('Solutions can also be in code cells.')"
+    ),
+    new_markdown_cell(
+        "## Other headers\n" +
+        "end the previous assignment."
+    ),
+])
 
 
 def name_to_dirname(name: str) -> str:
     return re.sub(r"\W+", "_", name.lower())
 
 
-def write_config(path: Path, name: str):
+def write_config(path: Path,
+                 config: Mapping[str, Any] = example_config) -> None:
     config_path = path.joinpath("config.yaml")
-    config = {
-        "name": name,
-        "semester": "semester",
-        "description": "description for your course",
-        "version": "0.1.0",
-        "authors": [
-            {
-                "name": "Your Name",
-                "email": "YourEmail@host.com",
-            }
-        ],
-        "git": {
-            "student": "Link to student repo",
-            "output_dir": "out",
-            "exclude": [
-                "config.yaml",
-                "container/",
-            ],
-        },
-    }
     yaml = YAML(typ="rt")
     try:
         with open(config_path, "w") as f:
@@ -45,61 +92,91 @@ def write_config(path: Path, name: str):
         raise click.FileError(str(config_path), str(e))
 
 
-def write_gitignore(path: Path):
+def write_gitignore(path: Path,
+                    content: str = example_gitignore
+                    ) -> None:
+
     file_path = path.joinpath(".gitignore")
-    file_content = textwrap.dedent(
-        """
-            out/
-            .ipynb_checkpoints/
-            .jupyter/
-            .vscode/
-            .DS_Store
-            """
-    ).strip()
     with open(file_path, "w") as f:
-        f.write(file_content)
+        f.write(content)
 
 
-def write_notebook(path: Path, config: dict):
-    cells = [
-        nbformat.v4.new_markdown_cell(
-            "# Example Notebook \n urnc detects cells by checking for keywords in markdown headers"
-        ),
-        nbformat.v4.new_code_cell("print('This is a normal code cell.')"),
-        nbformat.v4.new_markdown_cell(
-            "### Assignment 1 \n Assignments are detected by urnc."
-        ),
-        nbformat.v4.new_markdown_cell(
-            "Cells after an assignment header are considered part of the assignment."
-        ),
-        nbformat.v4.new_markdown_cell(
-            "### Solution \n Solutions are removed by the urnc ci command."
-        ),
-        nbformat.v4.new_code_cell(
-            "### Solution \n print('Solutions can also be in code cells.')"
-        ),
-        nbformat.v4.new_markdown_cell(
-            "## Other headers \n end the previous assignment."
-        ),
-    ]
-    nb = nbformat.v4.new_notebook(cells=cells)
+def write_example_notebook(path: Path,
+                           notebook: NotebookNode = example_notebook) -> None:
     nb_path = path.joinpath("example.ipynb")
     exporter = NotebookExporter()
-    notebook, _ = exporter.from_notebook_node(nb, {})
-    urnc.convert.write_notebook(notebook, nb_path, config)
+    content, _ = exporter.from_notebook_node(notebook, {})
+    with open(nb_path, "w", newline="\n") as f:
+        _ = f.write(content)
 
 
-def init(config, name):
-    dirname = name_to_dirname(name)
-    path = Path(config["base_path"]).joinpath(dirname)
-    if path.exists() and any(path.iterdir()):
-        raise click.UsageError(f"Directory {path} already exists as is not empty.")
+def init(name: str = "Example Course",
+         path: Union[str, None, Path] = None,
+         url: Union[str, None, Path] = None,
+         student_url: Union[str, None, Path] = None) -> git.Repo:
+    """
+    Initializes a new course repository with the following structure:
+
+    ```
+    <path>
+    ├── .git
+    ├── .gitignore
+    ├── config.yaml
+    └── example.ipynb
+    ```
+
+    Args:
+        name: The name of the course.
+        path: The directory path where the course will
+            be created. If not provided, the path is derived from the course
+            name.
+        url: The git URL for the upstream repository.
+            If a local file path ending in `.git` is provided, a bare repository
+            will be created at that location and configured as the origin
+            remote.
+        student_url: The git URL for the student repository.
+            If a local file path ending in `.git` is provided, a bare repository
+            will be created at that location and configured as the student
+            remote url in config.yaml.
+
+    Raises:
+        click.UsageError: If the target directory already exists and is not empty.
+        click.FileError: If there is an error writing the configuration file.
+
+    Returns:
+        git.Repo: The initialized git repository object.
+
+    Example:
+        >>> repo = init(name="My Example Course")
+        >>> print(repo.working_dir)
+        /path/to/my_example_course
+    """
+    if path is None:
+        path = Path(name_to_dirname(name))
+    if isinstance(path, str):
+        path = Path(path)
+    if os.path.exists(path) and any(os.scandir(path)):
+        raise click.UsageError(f"Directory {path} exists already and is not empty.")
+    config = example_config
+    if student_url is not None:
+        student_url = str(student_url)
+        if not ("@" in student_url or ":" in student_url):
+            os.makedirs(student_url, exist_ok=True)
+            git.Repo.init(student_url, bare=True, initial_branch="main")
+        config["git"]["student"] = student_url
+    config["name"] = name
     path.mkdir(parents=True, exist_ok=True)
     repo = git.Repo.init(path, initial_branch="main")
     write_gitignore(path)
-    write_config(path, name)
-    write_notebook(path, config)
+    write_config(path, config)
+    write_example_notebook(path)
     repo.git.add(all=True)
     repo.index.commit("Initial commit")
+    if url is not None:
+        url = str(url)
+        if not ("@" in url or ":" in url):
+            os.makedirs(url, exist_ok=True)
+            git.Repo.init(url, bare=True, initial_branch="main")
+        repo.create_remote("origin", url)
     log(f"Course {name} initialized at {path}")
     return repo
