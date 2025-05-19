754d8aa80a1c822c64374c39ed983c0943b81b70
Tobias Schmidt <tobias2.schmidt@ukr.de>
2025-05-19 13:04:55 +0200

    Add asserts to test_convert

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -1,15 +1,21 @@
-import os, tempfile
 import urnc
-from pathlib import Path
+import nbformat
 
-os.chdir(tempfile.mkdtemp())
+def test_convert():
+    # Init course containing solution cells
+    urnc.init.init(name="Test Course", path="test_course")
+    with open('test_course/example.ipynb', encoding="utf-8") as f:
+        nb = nbformat.read(f, as_version=4)
+    num_solution_cells = sum('## Solution' in (cell.source or '') for cell in nb.cells)
+    assert num_solution_cells > 0
 
-urnc.init.init()
-urnc.convert.convert(
-    config = urnc.config.default_config("example_course"),
-    input = Path('example_course/example.ipynb').absolute(),
-    targets = [{
-        "type": "student",
-        "path": "{nb.abspath}",
-    }]
-)
+    # Convert course to student version and check that solution cells are removed
+    config = urnc.config.default_config("test_course")
+    config["convert"]["write_mode"] = "overwrite"
+    input = 'test_course/example.ipynb'
+    targets = [{"type": "student", "path": "{nb.abspath}"}]
+    urnc.convert.convert(config, input, targets)
+    with open('test_course/example.ipynb', encoding="utf-8") as f:
+        nb = nbformat.read(f, as_version=4)
+    num_solution_cells = sum('## Solution' in (cell.source or '') for cell in nb.cells)
+    assert num_solution_cells == 0
