39baac29edad8472b3fca3ca556b269f036bbd79
Tobias Schmidt <tobias2.schmidt@ukr.de>
2025-05-19 17:53:46 +0200

    Add commands to live_coding_commands.bash

--- docs/internal/live_coding_commands.bash
+++ docs/internal/live_coding_commands.bash
@@ -1,2 +1,24 @@
+# 1. Initialize a New Course
+# ==========================
 urnc init --help
-urnc init --template=full
+urnc init --template=full "My Example Course"
+## Look at the files, source and gui --> Solution exists
+
+
+# 2. Create the Student Version
+# =============================
+
+# 3. Publish the Student Version
+# ==============================
+
+# 4. Pull the Student Version
+# ===========================
+
+# 5. Convert Notebooks to other targets
+# =====================================
+
+## Solution Target
+## Fix Target
+
+
+# 6. Check Notebooks
